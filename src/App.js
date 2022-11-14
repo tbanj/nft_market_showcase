ea5103d5df7727fa09a956a302183593a2343b91
Alabi Temitope <t.banji@rocketmail.com>
2022-11-14 17:45:14 +0100

    created boilerplate for nft showcase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Download, Features, SectionWrapper} from './components';
+import assets from './assets'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <SectionWrapper
+      title="You own store of Nifty NFTs, 
+      Start Selling & Growing"
+      description="Buy, store, collect NFTs,
+      exchange & earn crypto. Join 25+ 
+      million people using Pronef Marketplace."
+      showBtn
+      mockupImg={assets.homeHero}
+      banner="banner"
+    />
+    </>
   );
 }
 
---
+++ src/components/SectionWrapper.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import styles from '../styles/Global';
+import assets
+ from '../assets';
+const SectionWrapper = ({title, description, showBtn, mockupImg}) => {
+  return (
+    <div className={`min-h-screen ${styles.section}`}>
+        <h1>{title}</h1>
+        <h1>{description}</h1>
+        <img src={mockupImg} alt="mockup"/>
+    </div>
+  )
+}
+
+export default SectionWrapper
