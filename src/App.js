6d48ce8cf04aaf0e22392e5445a0fd7c25a4bb94
Alabi Temitope <t.banji@rocketmail.com>
2022-11-14 22:59:02 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Download, Features, SectionWrapper} from './components';
 import assets from './assets'
+import styles from './styles/Global';
 function App() {
   return (
     <>
@@ -15,15 +16,42 @@ function App() {
     />
 
     <SectionWrapper
-      title="You own store of Nifty NFTs, 
-      Start Selling & Growing"
-      description="Buy, store, collect NFTs,
-      exchange & earn crypto. Join 25+ 
-      million people using Pronef Marketplace."
-      showBtn
-      mockupImg={assets.homeHero}
-      banner="banner"
+      title="Smart User Interface Marketplace"
+      description="Experience a buttery UI of ProNef NFT Marketplace. 
+      Smooth constant colors of a fluent UI design."
+      
+      mockupImg={assets.homeCards}
+      reverse
     />
+     <Features/>
+     <SectionWrapper
+      title="Deployment"
+      description="ProfNef is built using Expo
+      which runs natively on all users'
+      devices. You can easily get your app into people's hands"
+      mockupImg={assets.feature}
+      reverse
+      
+    />
+
+    <SectionWrapper
+      title="Creative way to showcase the store"
+      description="The app contains two screens. The first screen list all
+      NFTs while the second one shows the details of a specific NFT"
+      
+      mockupImg={assets.mockup}
+      banner="banner02"
+    />
+    <Download/>
+
+
+    <div className='px-4 py-2 justify-center items-center bg-primary
+    flex-col text-center banner04'>
+      <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+      <span className='bold'>Alabi Wahab Temitope</span>
+
+      </p>
+    </div>
     </>
   );
 }
